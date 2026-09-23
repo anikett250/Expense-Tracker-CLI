403e4256f0d6fe43766e6d55bb7ece67d5589ea8
Aniket Tiwari <YOUR_NOREPLY_EMAIL>
2026-09-23 16:02:11 +0530

    added sorting

--- database.py
+++ database.py
@@ -38,6 +38,7 @@ def show_expense():
         dbname="anikettiwari",
         user="anikettiwari"
     ) as conn:
+        print()
         print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION'}")
         print("-" * 60)
 
@@ -52,3 +53,95 @@ def remove_expense(userdeleteid):
             user="anikettiwari"
     ) as conn:
         conn.execute("DELETE FROM expense_tracker WHERE id = %s", (userdeleteid,))
+        
+def update_expensedb(userupdateid, amount, category, description):
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        conn.execute("UPDATE expense_tracker SET price = %s, category = %s, description = %s WHERE id = %s", (amount, category, description, userupdateid))
+        print()
+        print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION'}")
+        print("-" * 60)
+        for row in conn.execute("SELECT * FROM expense_tracker"):
+            print(f"{row[0]:<5} {row[1]:<10} {row[2]:<15} {row[3]}")
+            
+def viewbyinc():
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        print()
+        print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION'}")
+        print("-" * 60)
+        for row in conn.execute("SELECT * FROM expense_tracker ORDER BY id ASC"):
+            print(f"{row[0]:<5} {row[1]:<10} {row[2]:<15} {row[3]}")
+            
+def viewbydesc():
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        print()
+        print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION'}")
+        print("-" * 60)
+        for row in conn.execute("SELECT * FROM expense_tracker ORDER BY id DESC"):
+            print(f"{row[0]:<5} {row[1]:<10} {row[2]:<15} {row[3]}")
+            
+def costsum():
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        total = conn.execute(
+            "SELECT SUM(price) FROM expense_tracker"
+        ).fetchone()[0]
+
+        print(f"Total Expenses: ₹{total}")
+        
+def filterbyfood():
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        print()
+        print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION'}")
+        print("-" * 60)
+        for row in conn.execute("SELECT * FROM expense_tracker WHERE category = 'Food'"):
+            print(f"{row[0]:<5} {row[1]:<10} {row[2]:<15} {row[3]}")
+            
+def filterbytransport():
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        print()
+        print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION'}")
+        print("-" * 60)
+        for row in conn.execute("SELECT * FROM expense_tracker WHERE category = 'Transport'"):
+            print(f"{row[0]:<5} {row[1]:<10} {row[2]:<15} {row[3]}")
+            
+def filterbyshopping():
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        print()
+        print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION'}")
+        print("-" * 60)
+        for row in conn.execute("SELECT * FROM expense_tracker WHERE category = 'Shopping'"):
+            print(f"{row[0]:<5} {row[1]:<10} {row[2]:<15} {row[3]}")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from database import add_expense
-from database import show_expense, remove_expense
+from database import add_expense, show_expense, remove_expense, update_expensedb, viewbyinc, viewbydesc, costsum, filterbyfood, filterbyshopping, filterbytransport
 
 def save_expense():
     amount = int(input("Enter Amount: "))
@@ -7,28 +6,61 @@ def save_expense():
     description = input("Description: ")
 
     add_expense(amount, category, description)
-    
-def view_expense():
-    print(show_expense())
 
 def delete_expense():
     userdeleteid = int(input("Which ID you want to delete: "))
     remove_expense(userdeleteid)
+    
+def update_expense():
+    userupdateid = int(input("Which data you want to update: "))
+    amtupdate = input("Update Amount: ")
+    catupdate = input("Update Category: ")
+    descupdate = input("Update Description: ")
+    update_expensedb(userupdateid, amtupdate, catupdate, descupdate)
 while True:
     print("\n1. Add Expense")
     print("2. View Expenses")
     print("3. Delete Expense")
-    print("4. Exit")
+    print("4. Update Expense")
+    print("5. Exit")
 
     useri = int(input("Enter choice: "))
 
     if useri == 1:
+        print(show_expense())
         save_expense()
     elif useri == 2:
-        view_expense()
+        print("\n1. View in Increasing Order")
+        print("2. View in Decreasing Order")
+        print("3. View Summary")
+        print("4. Sort by Category")
+
+        sort = int(input("Enter choice: "))
+
+        if sort == 1:
+            print(viewbyinc())
+        elif sort == 2:
+            print(viewbydesc())
+        elif sort == 3:
+            costsum()
+        elif sort == 4:
+            print("\n1. Food")
+            print("2. Transport")
+            print("3. Shopping")
+            userifilter = int(input("Enter choise: "))
+            if userifilter == 1:
+                print(filterbyfood())
+            elif userifilter == 2:
+                print(filterbytransport())
+            elif userifilter == 3:
+                print(filterbyshopping())
     elif useri == 3:
+        print(show_expense())
         delete_expense()
     elif useri == 4:
+        print(show_expense())
+        update_expense()
+    elif useri == 5:
         break
     else:
         print("Invalid choice.")
