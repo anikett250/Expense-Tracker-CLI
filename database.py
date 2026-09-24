47f5b0594ba12de699926e5e835d057f221d1c2b
Aniket Tiwari <202665250+anikett250@users.noreply.github.com>
2026-09-24 12:06:35 +0530

    budget storing and budget summaries

--- database.py
+++ database.py
@@ -18,6 +18,14 @@ with psycopg.connect(
             date DATE
         )
     """)
+    conn.execute(
+                """
+                CREATE TABLE IF NOT EXISTS budget(
+                    id SERIAL PRIMARY KEY,
+                    amount INT NOT NULL
+                )
+                """
+            )
 
 
 def add_expense(amount, category, description, today):
@@ -108,8 +116,20 @@ def costsum():
         total = conn.execute(
             "SELECT SUM(price) FROM expense_tracker"
         ).fetchone()[0]
+        foodtotal = conn.execute(
+            "SELECT SUM(price) FROM expense_tracker WHERE category = 'Food'"
+        ).fetchone()[0]
+        shoppingtotal = conn.execute(
+            "SELECT SUM(price) FROM expense_tracker WHERE category = 'Shopping'"
+        ).fetchone()[0]
+        traveltotal = conn.execute(
+            "SELECT SUM(price) FROM expense_tracker WHERE category = 'Travelling'"
+        ).fetchone()[0]
 
         print(f"Total Expenses: ₹{total}")
+        print(f"Total Food Expenses: ₹{foodtotal}")
+        print(f"Total Shopping Expenses: ₹{shoppingtotal}")
+        print(f"Total Travel Expenses: ₹{traveltotal}")
         
 def filterbyfood():
     with psycopg.connect(
@@ -228,3 +248,117 @@ def keywordsearch(keyword):
                     (f"%{keyword}%",)
                 ):
             print(f"{row[0]:<5} {row[1]:<10} {row[2]:<15} {row[3]:<15} {row[4]}")
+            
+def monthspend(spendstartmonth, spendendmonth):
+    startmonth = f"2026-{spendstartmonth:02d}-01"
+    endmonth = f"2026-{spendendmonth:02d}-30"
+
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+
+        total = conn.execute(
+            """
+            SELECT SUM(price)
+            FROM expense_tracker
+            WHERE date BETWEEN %s AND %s
+            """,
+            (startmonth, endmonth)
+        ).fetchone()[0]
+        print()
+        print(f"Total spending: ₹{total}")
+        
+def totalmonthspend():
+    startmonth = f"2026-{today.month:02d}-01"
+    endmonth = f"2026-{today.month:02d}-30"
+
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+
+        print()
+        print(f"{'ID':<5} {'PRICE':<10} {'CATEGORY':<15} {'DESCRIPTION':<15} {'DATE'}")
+        print("-" * 80)
+
+        for row in conn.execute(
+            """
+            SELECT *
+            FROM expense_tracker
+            WHERE date BETWEEN %s AND %s
+            ORDER BY date ASC
+            """,
+            (startmonth, endmonth)
+        ):
+            print(
+                f"{row[0]:<5} {row[1]:<10} {row[2]:<15} "
+                f"{row[3]:<15} {row[4]}"
+            )
+
+        total = conn.execute(
+            """
+            SELECT COALESCE(SUM(price), 0)
+            FROM expense_tracker
+            WHERE date BETWEEN %s AND %s
+            """,
+            (startmonth, endmonth)
+        ).fetchone()[0]
+
+        print(f"\nTotal Month Spending: ₹{total}")
+
+def storebudget(budget):
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        conn.execute(
+            """
+            INSERT INTO budget (id, amount)
+            VALUES (1, %s)
+            ON CONFLICT (id)
+            DO UPDATE SET amount = EXCLUDED.amount
+            """,
+            (budget,)
+        )
+
+def remainingbudget(budget):
+    startmonth = f"2026-{today.month}-01"
+    endmonth = f"2026-{today.month}-30"
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        total = conn.execute(
+            """
+            SELECT SUM(price) FROM expense_tracker WHERE date BETWEEN %s AND %s
+            """, (startmonth, endmonth)
+        ).fetchone()[0]
+        remaining = budget - total
+        print()
+        print(f"Remaining Budget: ₹{remaining}")
+        
+def getbudget():
+    with psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="anikettiwari",
+        user="anikettiwari"
+    ) as conn:
+        row = conn.execute(
+            """
+            SELECT amount
+            FROM budget
+            WHERE id = 1
+            """
+        ).fetchone()
+
+        return row[0] if row else 0
--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-from database import add_expense, show_expense, remove_expense, update_expensedb, viewbyinc, viewbydesc, costsum, filterbyfood, filterbyshopping, filterbytransport, today, sortbyyear, sortbymonth, sortbydate, keywordsearch
+import database as db
 
 def save_expense():
     amount = int(input("Enter Amount: "))
     category = input("Enter Category: ")
     description = input("Description: ")
 
-    add_expense(amount, category, description, today)
+    db.add_expense(amount, category, description, db.today)
 
 def delete_expense():
     userdeleteid = int(input("Which ID you want to delete: "))
-    remove_expense(userdeleteid)
+    db.remove_expense(userdeleteid)
     
 def update_expense():
     userupdateid = int(input("Which data you want to update: "))
     amtupdate = input("Update Amount: ")
     catupdate = input("Update Category: ")
     descupdate = input("Update Description: ")
-    update_expensedb(userupdateid, amtupdate, catupdate, descupdate)
+    db.update_expensedb(userupdateid, amtupdate, catupdate, descupdate)
 while True:
     print("\n1. Add Expense")
     print("2. View Expenses")
     print("3. Delete Expense")
     print("4. Update Expense")
-    print("5. Exit")
+    print("5. Set Budget")
+    print("6. Exit")
 
     useri = int(input("Enter choice: "))
 
     if useri == 1:
-        print(show_expense())
+        print(db.show_expense())
         save_expense()
     elif useri == 2:
         print("\n1. View in Increasing Order")
@@ -40,22 +41,31 @@ while True:
         sort = int(input("Enter choice: "))
 
         if sort == 1:
-            print(viewbyinc())
+            print(db.viewbyinc())
         elif sort == 2:
-            print(viewbydesc())
+            print(db.viewbydesc())
         elif sort == 3:
-            costsum()
+            print("1. Total Spending")
+            print("2. Monthly Spending")
+            userspend = int(input("Enter choice: "))
+            
+            if userspend == 1:
+                db.costsum()
+            elif userspend == 2:
+                spendstartmonth = int(input("Starting Month(1-12): "))
+                spendendmonth = int(input("Ending Month(1-12): "))
+                db.monthspend(spendstartmonth, spendendmonth)
         elif sort == 4:
             print("\n1. Food")
             print("2. Transport")
             print("3. Shopping")
             userifilter = int(input("Enter choise: "))
             if userifilter == 1:
-                print(filterbyfood())
+                print(db.filterbyfood())
             elif userifilter == 2:
-                print(filterbytransport())
+                print(db.filterbytransport())
             elif userifilter == 3:
-                print(filterbyshopping())
+                print(db.filterbyshopping())
         elif sort == 5:
             print("1. Sort by Year")
             print("2. Sort by Month")
@@ -66,13 +76,13 @@ while True:
             if sortdate == 1:
                 yearstart = int(input("Starting Year: "))
                 yearend = int(input("Ending Year: "))
-                print(sortbyyear(yearstart, yearend))
+                print(db.sortbyyear(yearstart, yearend))
             elif sortdate == 2:
                 monthyearstart = int(input("Starting Year: "))
                 monthyearend = int(input("Ending Year: "))
                 monthstart = int(input("Starting Month(1-12): "))
                 monthend = int(input("Ending Month(1-12): "))
-                print(sortbymonth(monthyearstart, monthyearend, monthstart, monthend))
+                print(db.sortbymonth(monthyearstart, monthyearend, monthstart, monthend))
             elif sortdate == 3:
                 dateyearstart = int(input("Starting Year: "))
                 dateyearend = int(input("Ending Year: "))
@@ -80,18 +90,31 @@ while True:
                 datemonthend = int(input("Ending Month(1-12): "))
                 datestart = int(input("Starting Date: "))
                 dateend = int(input("Ending Date: "))
-                print(sortbydate(dateyearstart, dateyearend, datemonthstart, datemonthend, datestart, dateend))
+                print(db.sortbydate(dateyearstart, dateyearend, datemonthstart, datemonthend, datestart, dateend))
         elif sort == 6:
             keyword = input("Enter Keyword: ")
-            print(keywordsearch(keyword))
+            print(db.keywordsearch(keyword))
             
     elif useri == 3:
-        print(show_expense())
+        print(db.show_expense())
         delete_expense()
     elif useri == 4:
-        print(show_expense())
+        print(db.show_expense())
         update_expense()
     elif useri == 5:
+        print("1. Show Total Month Spending")
+        print("2. Remaining Budget")
+        print("3. Set Budget")
+        userbudgeti = int(input("Enter choice: "))
+        
+        if userbudgeti == 1:
+            db.totalmonthspend()
+        elif userbudgeti == 2:
+            db.remainingbudget(budget)
+        elif userbudgeti == 3:
+            budget = int(input("Enter the Budget you want to set: "))
+            db.storebudget(budget)
+    elif useri == 6:
         break
     else:
         print("Invalid choice.")
